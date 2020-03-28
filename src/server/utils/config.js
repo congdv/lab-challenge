174a39737ca24bf310e3a954557348477d1eceae
Cong Dao <vdao8537@conestogac.on.ca>
2020-03-28 05:23:21 -0400

    Fix MongoDB URI

--- src/server/utils/config.js
+++ src/server/utils/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 let PORT = process.env.PORT || 3001;
+let MONGODB_URI = process.env.MONGODB_URI_DEV;
 switch (process.env.NODE_ENV) {
     case "production":
         MONGODB_URI = process.env.MONGODB_URI_PROD;
